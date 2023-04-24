23839e546974c13bf1227fd9b371a91a7c4f3808
abbe91 <abdullah.abo.alhaijja@hotmail.com>
2023-04-24 14:04:38 +0200

    Install bootstrap

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,8 @@
 function Message() {
   const name = "Abbe";
-  return <h1>Hello {name}</h1>;
+
+  if (name) return <h1>Hello {name}</h1>;
+
+  return <h1>hello world</h1>;
 }
 export default Message;
